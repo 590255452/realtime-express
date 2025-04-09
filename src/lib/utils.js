0d09677b91a0f10679295bedd7e53dfd0251bc1b
WangLuqi <wangluqi@WangLuqideMacBook-Pro.local>
2025-04-10 00:58:13 +0800

    remove

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true, // 防止XSS攻击
-        sameSite: "strict", // 防止CSRF攻击
+        sameSite: "none", // 防止CSRF攻击
         secure: process.env.NODE_ENV === "production", // 只有在生产环境中使用https传输cookie
     });
     return token;
